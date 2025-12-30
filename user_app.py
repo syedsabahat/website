5daf8cffd994b4702314e99b92e8eaadfb20a581
syedsabahat <qazisabahat100@gmail.com>
2025-12-30 14:41:55 +0530

    Update user_app.py

--- user_app.py
+++ user_app.py
@@ -66,6 +66,7 @@ def article_view(filename):
         content=content
     )
 
-
 if __name__ == "__main__":
-    app.run(debug=True, port=5050)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
+
